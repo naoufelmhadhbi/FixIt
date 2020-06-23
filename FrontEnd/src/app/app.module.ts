ba1f1ebadd89ff34a97e632f08483d2f572cfd60
aymenJeddey <49325025+aymenJeddey@users.noreply.github.com>
2020-06-23 23:26:28 +0100

    front

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SliderComponent } from './slider/slider.component';
+import { PublicationsComponent } from './publications/publications.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SliderComponent
+    SliderComponent,
+    PublicationsComponent
   ],
   imports: [
     BrowserModule
