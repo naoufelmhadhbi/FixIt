7645734d3872cee4fac3e89c2b241be7ba228c4c
IKB <Ibenjaballah@spb.eu>
2020-06-26 17:38:06 +0100

    getUserMessage

--- FixIt/src/MessagerieBundle/Controller/DefaultController.php
+++ FixIt/src/MessagerieBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace MessagerieBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use MessagerieBundle\Entity\Messagerie;
 
@@ -46,4 +47,42 @@ class DefaultController extends Controller
         $response = array('Code'=>1,'Success'=>'True' , 'Message'=>'message '.$message->getMessage().' a été envoyé');
         return new JsonResponse($response);
     }
+
+    /**
+     * @Route("/getMessagesByUser", name="listMessageByUser")
+     */
+    public function getMessagesByUser(Request $request){
+        $idDemandeur = $request->query->get('id_demandeur') ;
+        $idProfessionnel = $request->query->get('id_professionnel') ;
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery('SELECT c FROM MessagerieBundle:Messagerie c');
+        if($idDemandeur != null)
+            $query = $em->createQuery('SELECT c FROM MessagerieBundle:Messagerie c where c.idDemandeur = '.$idDemandeur);
+        if($idProfessionnel != null)
+            $query = $em->createQuery('SELECT c FROM MessagerieBundle:Messagerie c where c.idProfessionnel = '.$idProfessionnel);
+        if($idProfessionnel != null && $idDemandeur != null)
+            $query = $em->createQuery('SELECT c FROM MessagerieBundle:Messagerie c where c.idProfessionnel = '.$idProfessionnel.' and c.idDemandeur = '.$idDemandeur);
+
+        $users = $query->getArrayResult();
+        if (empty($users)) {
+            $response = array(
+                'code' => 1,
+                'message' => 'User Not found !',
+                'errors' => null,
+                'result' => null
+            );
+            return new JsonResponse($response, Response::HTTP_NOT_FOUND);
+        }
+        $response = new Response();
+        $response->setContent(json_encode($users));
+        $response->headers->set('Content-Type', 'application/json');
+    // Allow all websites
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        //$users->header->set('Access-Control-Allow-Origin', '*');
+        return $response;//new JsonResponse($users);
+    }
+
+
+
 }
